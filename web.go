d8bb421655be6ffc42cc7ebb06f788585ecc489f
Алексей Елагин <a.elagin@corp.youdo.com>
2018-03-20 18:51:02 +0300

    auth form

--- web.go
+++ web.go
@@ -5,3 +5,10 @@ import "net/http"
 var DumbHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Dumb"))
 })
+
+var RootHandler = http.HandlerFunc(RootRoute)
+
+func RootRoute(w http.ResponseWriter, r *http.Request) {
+
+
+}
